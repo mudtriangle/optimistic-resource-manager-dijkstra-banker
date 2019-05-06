f1af990665a89adea212464da9beb7dc97d77dee
Marcelo Sandoval-Castañeda <43766404+mudtriangle@users.noreply.github.com>
2019-05-06 10:18:13 +0400

    Fixed a minor typo

--- resmanager.cpp
+++ resmanager.cpp
@@ -797,7 +797,7 @@ int main(int argc, char** argv) {
     if (argc == 1) {
         cerr << "No input file given." << endl;
         return 1;
-    } else if (argc == 2) {
+    } else if (argc >= 2) {
         file = argv[1];
     }
 
